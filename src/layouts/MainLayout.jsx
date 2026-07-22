6a588e1d3b6965c377975b4cb649b147f3133e67
Archana <sakthisiva8486@gmail.com>
2026-07-22 18:25:48 +0530

    refactor: update main layout structure

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,9 @@
-import { Outlet } from "react-router-dom";
-import Header from "../components/Navbar";
+import Navbar from "../components/Navbar";
 
 const MainLayout = () => {
     return (
         <>
-            <Header />
-            <Outlet />
+            <Navbar />
         </>
     );
 };
