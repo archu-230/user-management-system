1c7ca799d6d927a4e67299497243d732917bb568
Archana <sakthisiva8486@gmail.com>
2026-07-22 18:25:36 +0530

    feat: add theme toggle using mui color scheme hook

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,34 @@
-import { AppBar, IconButton, Typography, } from "@mui/material";
+import { AppBar, IconButton } from "@mui/material";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
-import { useThemeMode } from "../context/ThemeContext";
+
+import useThemeMode from "../theme/useThemeMode";
 
 const Header = () => {
+
     const { mode, toggleTheme } = useThemeMode();
 
     return (
-        <AppBar position="fixed" color="default" >
-            <IconButton onClick={toggleTheme} color="inherit" disableRipple
+        <AppBar position="fixed" color="default">
+
+            <IconButton
+                onClick={toggleTheme}
+                color="inherit"
+                disableRipple
                 sx={{
-                    p: 1, m: 1, borderRadius: "50%"
-                }}>
-                {mode === "dark" ? <Brightness7Icon /> : <Brightness4Icon />}
+                    p: 1,
+                    m: 1,
+                    borderRadius: "50%"
+                }}
+            >
+                {
+                    mode === "dark"
+                        ? <Brightness7Icon />
+                        : <Brightness4Icon />
+                }
+
             </IconButton>
+
         </AppBar>
     );
 };
