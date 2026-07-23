3738e1df3f147a530ade282c26cdd5f4c5d3988e
Archana <sakthisiva8486@gmail.com>
2026-07-23 14:50:30 +0530

    refactor(app): add reusable app bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { BrowserRouter } from "react-router-dom";
 import MUIProvider from "./providers/MUI";
-import Header from "./components/Navbar";
+import Header from "./components/AppBar";
+import Breadcrumbs from "./components/Breadcrumbs";
 
 const App = () => {
   return (
-    <MUIProvider>
-      <Header />
-    </MUIProvider>
+    <BrowserRouter>
+      <MUIProvider>
+        <Header />
+        <Breadcrumbs
+          items={[
+            { label: "Home", path: "/" },
+            { label: "Products", path: "/products" },
+            { label: "Items", path: "/items" }
+          ]}
+        />
+      </MUIProvider>
+    </BrowserRouter>
   );
 };
 
