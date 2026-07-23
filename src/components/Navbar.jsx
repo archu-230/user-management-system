3cd4728afe6c5c0b31bddabeb19b7cc86e1a509b
Archana <sakthisiva8486@gmail.com>
2026-07-23 12:40:06 +0530

    refactor(theme): integrate reusable theme toggle component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,10 @@
-import { AppBar, IconButton } from "@mui/material";
-import Brightness4Icon from "@mui/icons-material/Brightness4";
-import Brightness7Icon from "@mui/icons-material/Brightness7";
-import MUIProvider, { useThemeMode } from "../provider/MUI";
+import { AppBar } from "@mui/material";
+import ThemeToggleButton from "./ThemeToggle";
 
 const Header = () => {
-
-    const { mode, toggleTheme } = useThemeMode();
-
     return (
         <AppBar position="fixed" color="default">
-
-            <IconButton
-                onClick={toggleTheme}
-                color="inherit"
-                disableRipple
-                sx={{
-                    p: 1,
-                    m: 1,
-                    borderRadius: "50%"
-                }}
-            >
-                {
-                    mode === "dark"
-                        ? <Brightness7Icon />
-                        : <Brightness4Icon />
-                }
-
-            </IconButton>
-
+            <ThemeToggleButton />
         </AppBar>
     );
 };
