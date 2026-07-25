7e890d7c07527c55b8ffb0888309b8b52d2e1efd
Archana <sakthisiva8486@gmail.com>
2026-07-25 18:10:38 +0530

    refactor: update breadcrumbs for object based config

---
+++ src/components/app-breadcrumbs/index.jsx
@@ -0,0 +1,41 @@
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+
+import { Link as RouterLink, useLocation, matchPath } from "react-router-dom";
+
+import config from "./config";
+
+const AppBreadcrumb = () => {
+    const { pathname } = useLocation();
+
+    const currentBreadcrumb = Object.values(config)
+        .flatMap((module) => module.config)
+        .find((item) =>
+            matchPath({ path: item.path, end: true }, pathname)
+        );
+
+    return (
+        <Breadcrumbs
+            separator={<ChevronRightIcon fontSize="small" />}
+            sx={{ p: 2 }} >
+            <Link
+                component={RouterLink}
+                underline="none"
+                color="inherit"
+                to="/"
+            >
+                Home
+            </Link>
+
+            {currentBreadcrumb && pathname !== "/" && (
+                <Typography color="text.primary">
+                    {currentBreadcrumb.label}
+                </Typography>
+            )}
+        </Breadcrumbs>
+    );
+};
+
+export default AppBreadcrumb;
