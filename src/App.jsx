1258e80249e47d61f511caebee8af55521ef46f4
Archana <sakthisiva8486@gmail.com>
2026-07-25 15:03:19 +0530

    feat: add AppBreadcrumb to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-  
-import { Toolbar } from "@mui/material";
+
+import { Breadcrumbs, Toolbar } from "@mui/material";
 import { useState } from "react";
 import MUIProvider from "./providers/MUI";
 import AppBoard from "./components/app-board";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Profile from "./pages/Profile";
 import Bar from "./components/navbar"
+import breadcrumbConfig from "./components/app-breadcrumb/config";
+import AppBreadcrumb from "./components/app-breadcrumb";
 
 const App = () => {
   const [open, setOpen] = useState(false);
@@ -13,13 +15,15 @@ const App = () => {
   return (
     <BrowserRouter>
       <MUIProvider>
-         <Bar onclickMenu={()=>setOpen(prev=>!prev)}/>
+
+        <Bar onclickMenu={() => setOpen(prev => !prev)} />
         <Toolbar />
         <AppBoard open={open} />
-          <Routes>
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/" element={<></>}/>
-          </Routes>
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/" element={<></>} />
+        </Routes>
+        <AppBreadcrumb />
       </MUIProvider>
     </BrowserRouter>
   );
