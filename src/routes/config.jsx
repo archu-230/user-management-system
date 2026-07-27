ff2a08a7078d6fd328a8c64a752dcc0582f3fe61
Vijay balaji <balajibaskaran456@gmail.com>
2026-07-27 10:51:55 +0530

    feat: implement custom route configuration

---
+++ src/routes/config.jsx
@@ -0,0 +1,13 @@
+import Profile from "../pages/Profile";
+
+export const routes = [
+    
+    {
+        path:"/profile",
+        element:<Profile/>,
+    },
+    {
+        path:"/",
+        element:<></>
+    },
+];
