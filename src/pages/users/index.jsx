d374dd138c3103097f33fdda3c06fc3adc952bde
Archana <sakthisiva8486@gmail.com>
2026-07-30 18:17:46 +0530

    refactor: update user table with create-user-dialog

--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -1,10 +1,35 @@
+import { useState } from "react";
+
 import AddIcon from "@mui/icons-material/Add";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { Box, Button } from "@mui/material";
+
 import DataGrid from "../../components/data-grid";
-import { columns, rows } from "./config";
+import { columns } from "./config";
+import CreateUserDialog from "./Create-user-dialog";
 
 export default function Users() {
+  const [open, setOpen] = useState(false);
+  const [rows, setRows] = useState([]);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleAddUser = (user) => {
+    const newUser = {
+      id: rows.length + 1,
+      name: user.name,
+      email: user.email,
+    };
+
+    setRows((prev) => [...prev, newUser]);
+  };
+
   return (
     <Box
       sx={{
@@ -24,9 +49,11 @@ export default function Users() {
         }}
       >
         <Button
+          id="new-user-btn"
           variant="outlined"
           startIcon={<AddIcon />}
           sx={{ textTransform: "none" }}
+          onClick={handleOpen}
         >
           New
         </Button>
@@ -42,7 +69,17 @@ export default function Users() {
         </Button>
       </Box>
 
-      <DataGrid rows={rows} columns={columns} checkbox={true}/>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        checkbox
+      />
+
+      <CreateUserDialog
+        open={open}
+        onClose={handleClose}
+        onSave={handleAddUser}
+      />
     </Box>
   );
 }
