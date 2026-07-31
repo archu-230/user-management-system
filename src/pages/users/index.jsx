377493a355171c8e2ccdbd59ea887a1b5dd35791
Archana <sakthisiva8486@gmail.com>
2026-07-31 14:41:34 +0530

    fix: replace datagrid import with datatable import

--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -4,7 +4,6 @@ import AddIcon from "@mui/icons-material/Add";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { Box, Button } from "@mui/material";
 
-import DataGrid from "../../components/data-grid";
 import { columns } from "./config";
 import CreateUserDialog from "./components/Create-user-dialog";
 import DataTable from "../../components/data-table";
