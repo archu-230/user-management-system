ba9f580148beab051f7a5728eb19283c7ee51ae9
Vijay balaji <balajibaskaran456@gmail.com>
2026-08-01 12:03:07 +0530

    refactor: remove unused import statement

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import MUIProvider from "./providers/MUI";
 import router from "./routes";
-import { Box, Button, TextField } from "@mui/material";
-import { useState } from "react";
-
 
 const App = () => {
 
